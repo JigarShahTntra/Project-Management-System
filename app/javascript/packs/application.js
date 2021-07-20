c2ddf5cee4b12fa0ed95ba092972c8b93e609587
Urvashi Vasita <urvashi.vasita23@gmail.com>
2021-07-20 15:00:10 +0530

    Modigied the Gemfile : installed Devise gem and Devise Invitable , added client side validation in JS file.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,10 +6,13 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-require("jquery")
-import "bootstrap"
-import "../stylesheets/application"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+require("channels");
+require("jquery");
+import "bootstrap"
+import "../stylesheets/application"
+require("packs/jquery-validate");
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/index'
+    get 'users/check_email', to: 'users#check_email'
+  end
+  devise_for :users
+  root 'admin/users#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
