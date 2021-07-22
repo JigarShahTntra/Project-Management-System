e6d57a320c3185e2faab2bac2928fb4381173450
Urvashi Vasita <urvashi.vasita23@gmail.com>
2021-07-22 11:19:25 +0530

    Generated views for admin , integrated devise and SMTP

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,10 @@
+class Admin::UsersController < ApplicationController
+  def index
+  end
+  # def check_email
+  #   @user = User.find_by_email(params[:user][:email])
+
+  #   respond_to do |format|
+  #    format.json { render :json => !@user }
+  #   end
+end
