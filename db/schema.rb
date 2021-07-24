f2e81b1356dcdb53346f45ef778ae85bf976d37c
Urvashi Vasita <urvashi.vasita23@gmail.com>
2021-07-24 13:54:53 +0530

    adding schems.rb , javascript files for validation , generated client model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,138 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_155439) do
+ActiveRecord::Schema.define(version: 2021_07_22_064851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "employees_roles", force: :cascade do |t|
+    t.bigint "employee_id"
+    t.bigint "role_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["employee_id"], name: "index_employees_roles_on_employee_id"
+    t.index ["role_id"], name: "index_employees_roles_on_role_id"
+  end
+
+  create_table "employees_teams", force: :cascade do |t|
+    t.bigint "team_id"
+    t.bigint "employee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["employee_id"], name: "index_employees_teams_on_employee_id"
+    t.index ["team_id"], name: "index_employees_teams_on_team_id"
+  end
+
+  create_table "employees_technologies", force: :cascade do |t|
+    t.bigint "technology_id"
+    t.bigint "employee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["employee_id"], name: "index_employees_technologies_on_employee_id"
+    t.index ["technology_id"], name: "index_employees_technologies_on_technology_id"
+  end
+
+  create_table "financials", force: :cascade do |t|
+    t.string "bank_name"
+    t.string "ifsc"
+    t.string "micr"
+    t.text "bank_address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "profile_id", null: false
+    t.index ["profile_id"], name: "index_financials_on_profile_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.bigint "subject_id"
+    t.bigint "project_user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_user_id"], name: "index_notifications_on_project_user_id"
+    t.index ["subject_id"], name: "index_notifications_on_subject_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders_statuses", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "status_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_orders_statuses_on_order_id"
+    t.index ["status_id"], name: "index_orders_statuses_on_status_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "prof_type"
+    t.integer "experience"
+    t.string "position"
+    t.string "aadhar_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "profilable_type", null: false
+    t.bigint "profilable_id", null: false
+    t.index ["profilable_type", "profilable_id"], name: "index_profiles_on_profilable"
+  end
+
+  create_table "project_employees", force: :cascade do |t|
+    t.string "responsibilities"
+    t.bigint "employee_id"
+    t.bigint "project_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["employee_id"], name: "index_project_employees_on_employee_id"
+    t.index ["project_id"], name: "index_project_employees_on_project_id"
+  end
+
+  create_table "project_users", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "project_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_project_users_on_project_id"
+    t.index ["user_id"], name: "index_project_users_on_user_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "duration"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "team_id", null: false
+    t.bigint "client_id", null: false
+    t.index ["client_id"], name: "index_projects_on_client_id"
+    t.index ["team_id"], name: "index_projects_on_team_id"
+  end
+
+  create_table "revenues", force: :cascade do |t|
+    t.decimal "revenue_percent", precision: 4, scale: 2
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "project_id", null: false
+    t.index ["project_id"], name: "index_revenues_on_project_id"
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "role"
@@ -22,12 +150,37 @@ ActiveRecord::Schema.define(version: 2021_07_19_155439) do
   end
 
   create_table "roles_users", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "role_id", null: false
+    t.bigint "role_id"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["role_id"], name: "index_roles_users_on_role_id"
     t.index ["user_id"], name: "index_roles_users_on_user_id"
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.integer "index"
+    t.string "notification_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "responsibilities"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "technologies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,6 +207,8 @@ ActiveRecord::Schema.define(version: 2021_07_19_155439) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "roles_users", "roles"
-  add_foreign_key "roles_users", "users"
+  add_foreign_key "financials", "profiles"
+  add_foreign_key "projects", "clients"
+  add_foreign_key "projects", "teams"
+  add_foreign_key "revenues", "projects"
 end
