18c2b5bdd7a01685900b562e65af099574aa5929
Urvashi Vasita <urvashi.vasita23@gmail.com>
2021-07-26 17:35:59 +0530

    modified

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# user module
+# Admin-module
 module Admin
   # user-controller
   class UsersController < ApplicationController
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@
 
 # User-model
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :invitable
   has_and_belongs_to_many :roles
