d1c2a7e1308baae36cf867fa81b14c749189a80b
Urvashi Vasita <urvashi.vasita23@gmail.com>
2021-07-30 15:29:43 +0530

    Modified environemt files and controllers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,6 @@
 
 //= require rails-ujs
 //= require activestorage
+//= require jquery.min
+//= require jquery-validation.min
+//= require form_validation
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,4 @@
+//= require rails-ujs
 //= require jquery.min.js
 //= require jquery-validate.js
 //= require login.js
